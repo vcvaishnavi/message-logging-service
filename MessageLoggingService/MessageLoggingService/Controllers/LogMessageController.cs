394d7bf6bc2ba6209e30fde2fb12002002b0cb13
Vaishnavi <vcvaishnavi@gmail.com>
2023-03-06 10:19:52 +0100

    Added Rest Method for setMaxAge
    Co-Authored-By: vaishnavi <346932+Vaishnavi@users.noreply.github.com>

--- MessageLoggingService/MessageLoggingService/Controllers/LogMessageController.cs
+++ MessageLoggingService/MessageLoggingService/Controllers/LogMessageController.cs
@@ -3,6 +3,7 @@ using MessageLoggingService.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
 
 namespace MessageLoggingService.Controllers
 {
@@ -16,18 +17,49 @@ namespace MessageLoggingService.Controllers
             _logRepository= logRepository;
         }
 
+        
         [HttpGet(Name = "GetLog")]
+
+        /// <summary>
+        /// Reads the log messages from the log object and filters it by log ID and MaxAge
+        /// </summary>
+        /// <param name="logId"></param>
+        /// <returns>List of log messages after filter</returns>
         public List<Log> GetLog(int logId)
         {
            
             return (_logRepository.getLog(logId));
         }
 
+       
         [HttpPost(Name = "AddMessage")]
+        /// <summary>
+        /// Adds the log message to the log object.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="logId"></param>
+        /// <param name="message"></param>
         public void AddMessage(string name,int logId,string message)
         {
 
             _logRepository.addMessage(name, logId, message);
+        }
+       
+      
+        [HttpPut(Name = "SetMaxAge")]
+        /// <summary>
+        /// Updates the max Age variable to the given value.
+        /// </summary>
+        /// <param name="maxAge"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public void SetMaxAge([Required] int maxAge)
+        {
+            if(maxAge == 0)
+            {
+                throw new ArgumentException("MaxAge cannot be 0.");
+            }
+
+            _logRepository.setMaxAge(maxAge);
         }
 
     }
--- MessageLoggingService/MessageLoggingService/Repositories/LogRepository.cs
+++ MessageLoggingService/MessageLoggingService/Repositories/LogRepository.cs
@@ -11,46 +11,25 @@ namespace MessageLoggingService.Repositories
 {
     public class LogRepository : ILogRepository
     {
-        protected const string fileName = "Log.json";
-      //  protected string appParameterFilename = AppDomain.CurrentDomain.BaseDirectory + "\\AppParameters.json";
+     
       private AppParameters _appParameters;
+        private List<Log> _logs;
         public LogRepository()
         {
             _appParameters = new AppParameters();
+            _logs = new List<Log>();
         }
         public List<Log> getLog([Required] int logId)
         {
-            List<Log> source = new List<Log>();
-
-            var json = File.ReadAllText(fileName);
-            if (!(string.IsNullOrEmpty(json)))       
-                    source = JsonSerializer.Deserialize<List<Log>>(json);
-
-
-            if (source != null)
-            {
-                return source.Where(x => x.logId == logId)
-                    .Where(x => (DateTime.Now- x.loggedAt).TotalSeconds < _appParameters.maxAge).ToList();                    
-            }
-            else
-                return null;
+            return _logs.Where(x => x.logId == logId)
+                    .Where(x => (DateTime.Now - x.loggedAt).TotalSeconds < _appParameters.maxAge).ToList();
         }
 
         public void addMessage([Required] string name, [Required] int logId, string message)
         {
 
             var logMessage = new Log { logId = logId, message = message, name = name, loggedAt = DateTime.Now };
-            List<Log> source = new List<Log>();
-
-            var json = File.ReadAllText(fileName);
-
-            if (!(string.IsNullOrEmpty(json)))
-                source = JsonSerializer.Deserialize<List<Log>>(json);
-
-            source.Add(logMessage);
-            string jsonString = JsonSerializer.Serialize(source, new JsonSerializerOptions() { WriteIndented = true });
-            File.WriteAllText(fileName, jsonString);
-                
+            _logs.Add(logMessage);            
 
         }
 
